b2a3b4252989f455e02c9c39aea2b3d32975eb60
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-07-16 02:05:05 +0900

    Add api/v1/index.js

---
+++ api/v1/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var CommentModel = require('./models/commentModel.js')
+
+const connectionOption = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/ExpressAPI', connectionOption);
+mongoose.connection.on('error', function(err) {
+    console.error("MongoDB connection error: " + err);
+    process.exit(1);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var port = process.env.PORT || 3000;
+var router = require('./routes');
+
+app.use('/api/v1', router);
+app.listen(port)
+
+console.log("listen on port " + port);
