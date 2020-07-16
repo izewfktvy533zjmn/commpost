df3915f1e9e5a5fa4db1f584f556e73ae45d25b9
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-07-16 17:22:52 +0900

    Implement health check

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -2,5 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/comments', require('./commentRoute.js'));
+router.use('/healthz', require('./healthzRoute.js'));
 
 module.exports = router;
