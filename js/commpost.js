8d18f823d9b84bbf8f8cd9305c9352e53c26d796
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-07-18 04:19:41 +0900

    Create homepage for commpost

---
+++ js/commpost.js
@@ -0,0 +1,107 @@
+window.addEventListener('DOMContentLoaded', async function() {
+    var url = 'https://commpost.on-going.jp/api/v1/healthz';
+    var method = 'GET';
+    var uri = document.documentURI;
+    var article_id = await digestMessage(uri);
+    
+    timeout(3000, fetch(url, {
+        method
+    })).then(function(data) {
+        return data.json();
+    }).then(function (json) {
+        if (json['status'] == "ok") {
+            var promise = getComment(article_id);
+            
+            promise.then(function(json) {
+                target = document.getElementById('commpost');
+                target.innerHTML = 'Commpost <br> <table id="display_comment">Getting comments...</table> <form id="comment_form" action="#" onsubmit="return false;"> <p>Name: <br> <input type="text" id="input_name" placeholder="Anonymous"> </p> <p>Comment: <br> <textarea id="input_comment"></textarea> </p> <button type="button" onclick="sendComment();">Send</button> </form>';
+                showComment(json);
+
+            });
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
+
+});
+
+function timeout(ms, promise) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            reject(new Error("timeout"));
+        }, ms);
+        promise.then(resolve, reject);
+    });
+}
+
+
+async function digestMessage(message){
+    const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+
+    return hashHex;
+}
+
+function showComment(json) {
+    for (var i = 0; i < json.length; i++) {
+        var date = json[i].date;  
+        var poster_name = json[i].poster_name;
+        var text = json[i].text;
+        var row = document.getElementById('display_comment').insertRow();
+
+        row.insertCell().textContent = date;
+        row.insertCell().textContent = poster_name;
+        row.insertCell().textContent = text;
+    }
+}
+
+function getComment(article_id) {
+    var url = 'https://commpost.on-going.jp/api/v1/comments?article_id=' + article_id;
+    var method = 'GET';
+
+    return fetch(url, {
+        method
+    }).then(function(data) {
+        return data.json();
+    }).then(function (json) {
+        return json;
+    }).catch((err) => {
+        return {};
+    });
+}
+
+async function sendComment() {
+    var uri = document.documentURI;
+    var article_id = await digestMessage(uri);
+    var name = document.getElementById("input_name").value;
+    var comment = document.getElementById("input_comment").value;
+    if (name.length == 0) name = "Anonymous";
+    if (comment.length == 0) return;
+
+    var url = 'https://commpost.on-going.jp/api/v1/comments';
+    var obj = {
+        "article_id": article_id,
+        "poster_name": name,
+        "text": comment
+    };
+    var method = 'POST';
+    var body = JSON.stringify(obj);
+    var headers = {
+      'Content-Type': 'application/json'
+    };
+
+    fetch(url, {
+        method, 
+        headers, 
+        body
+    }).then((data) => {
+        return data.json()
+    }).then((json) => {
+        showComment([json]);
+        document.forms['comment_form'].reset();
+    }).catch((err) => {
+        console.log(err);
+    });
+}
